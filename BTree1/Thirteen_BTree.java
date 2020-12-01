f53c41900fc63937b1826fadee5f107ea999f187
Aman Jakkani <aj280598@gmail.com>
2020-12-01 11:46:32 -0600

    init bomb

---
+++ BTree1/Thirteen_BTree.java
@@ -0,0 +1,214 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class Thirteen_BTree<Key extends Comparable<Key>>  {
+    //change order here
+    private static final int M = 13;
+    private Node root;       
+    private int height;      
+    private int n;           
+
+    //nodes class
+    private static final class Node{
+        //num of children
+        private int c;                
+        //array of children             
+        private Entry[] children = new Entry[M];  
+        private Node(int x){
+            c = x;
+        }
+    }
+
+    //entry class
+    private static class Entry{
+        private Comparable key;
+        private Node next;     
+        public Entry(Comparable key, Node next) {
+            this.key  = key;
+            this.next = next;
+        }
+    }
+
+    //initializer
+    public Thirteen_BTree(){
+        root = new Node(0);
+    }
+ 
+    //helper functions
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+    public int height(){
+        return height;
+    }
+    public int size(){
+        return n;
+    }
+    private boolean less_than(Comparable k1, Comparable k2){
+        return k1.compareTo(k2) < 0;
+    }
+
+    private boolean equal_to(Comparable k1, Comparable k2){
+        return k1.compareTo(k2) == 0;
+    }
+    //split node in half
+    private Node split(Node h){
+        Node t = new Node(M/2);
+        h.c = M/2;
+        for (int j = 0; j < M/2; j++)
+            t.children[j] = h.children[M/2+j]; 
+        return t;    
+    }
+
+    //adding to tree
+    public void put(Key key){
+        if (key == null) throw new IllegalArgumentException("argument is not valid; null");
+        Node u = insert(root, key, height); 
+        n++;
+        if (u == null) return;
+        //split if inserted is not null
+        Node t = new Node(2);
+        t.children[0] = new Entry(root.children[0].key, root);
+        t.children[1] = new Entry(u.children[0].key, u);
+        root = t;
+        height++;
+    }
+
+    private Node insert(Node h, Key key, int height){
+        int j;
+        Entry t = new Entry(key, null);
+        //if reached level 0
+        if(height == 0){
+            for(j = 0; j < h.c; j++){
+                if(less_than(key, h.children[j].key)) break;
+            }
+        }
+        else{
+            for(j = 0; j < h.c; j++){
+                if((j+1 == h.c) || less_than(key, h.children[j+1].key)) {
+                    System.out.println(h.children[j].key);
+                    Node u = insert(h.children[j++].next, key, height-1);
+                    if (u == null) return null;
+                    t.key = u.children[0].key;
+                    t.next = u;
+                    break;
+                }
+            }
+        }
+        for (int i = h.c; i > j; i--){
+            h.children[i] = h.children[i-1];
+        }
+        h.children[j] = t;
+        h.c++;
+        if(h.c < M) return null;
+        else return split(h);
+    }
+
+    public boolean get(Key key){
+        if(key == null) throw new IllegalArgumentException("argument is not valid; null");
+        return search(root, key, height);
+    }
+
+    //recursive searching
+    private boolean search(Node x, Key key, int height){
+        Entry[] children = x.children;
+        if(height == 0){
+            for(int j = 0; j < x.c; j++){
+                if(equal_to(key, children[j].key)) return true;
+            }
+        }
+        else{
+            for(int j = 0; j < x.c; j++){
+                if(j+1 == x.c || less_than(key, children[j+1].key)){
+                    System.out.println(children[j].key);
+                    return search(children[j].next, key, height-1);
+                }
+                    
+            }
+        }
+        return false;
+    }
+
+    public void delete(Key key){
+        if(key == null) throw new IllegalArgumentException("argument is not valid; null");
+        if(search(root, key, height)) System.out.println("Key was found and deleted!");
+        else System.out.println("Key was not found, and thus not deleted");
+        
+    }
+
+    //convert tree to string for outputting
+    public String toString() {
+        return toString(root, height, "") + "\n";
+    }
+    private String toString(Node h, int ht, String indent){
+        StringBuilder sb = new StringBuilder();
+        Entry[] children = h.children;
+
+        if (ht == 0){
+            for (int j = 0; j < h.c; j++) {
+                sb.append(indent + children[j].key + "\n");
+            }
+        }
+        else{
+            for(int j = 0; j < h.c; j++){
+                if (j > 0) sb.append(indent + "(" + children[j].key + ")\n");
+                sb.append(toString(children[j].next, ht-1, indent + "     "));
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        Thirteen_BTree<String> bt = new Thirteen_BTree<String>();
+        Set<Integer> check = new HashSet<Integer>();
+        Random rand = new Random();
+        System.out.println("Starting data generation...");
+        System.out.println();
+        while(check.size() < 10000){
+            int randInt = rand.nextInt(100000) + 100000;
+            if(check.add(randInt)) bt.put(String.valueOf(randInt));
+        }
+        System.out.println("Data Generation Done!");
+        System.out.println();
+        System.out.println("size:    " + bt.size());
+        System.out.println("height:  " + bt.height());
+        System.out.println();
+
+        //randomn insertion and deletion operations
+        int i = 0;
+        while(i < 5){
+            int randInt = rand.nextInt(100000) + 100000;
+            if(check.add(randInt)){
+                System.out.println("Inserting "+randInt+"...");
+                i++;
+                System.out.println("Nodes involved: ");
+                bt.put(String.valueOf(randInt));
+                System.out.println();
+            }
+        }
+        while(i > 0){
+            int randInt = rand.nextInt(100000) + 100000;
+            if(check.remove(randInt)){
+                System.out.println("Deleting "+randInt+"...");
+                i--;
+                bt.delete(String.valueOf(randInt));
+                System.out.println();
+            }
+        }
+
+        //random search operations, guaranteeing at least three finds
+        while(i < 3){
+            int randInt = rand.nextInt(100000) + 100000;
+            System.out.println("Searching for "+randInt+"...");
+            System.out.println("Nodes involved: ");
+            boolean isThere = bt.get(String.valueOf(randInt));
+            if(isThere){
+                System.out.println("Found "+randInt+"!");
+                i++;
+            }
+            else System.out.println(randInt+" does not exist in this tree.");
+            System.out.println();
+        }
+    }
+
+}
---
+++ Join/Join_Simulation.java
@@ -0,0 +1,354 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Random;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Join_Simulation{
+
+    //global variable  
+    public static final int NUM_OF_BLOCKS_IN_MEMORY = 15; 
+
+    //tuple class
+    public static class Tuple implements Serializable{
+        private int[] fields;
+        private int relation_index;
+        public Tuple(){
+            this.fields = new int[2];
+            this.relation_index = -1;
+        }
+        public Tuple(Tuple t){
+            this.fields = new int[2];
+            this.fields = t.getFields();
+            this.relation_index = t.getRelIndex();
+        }
+        public Tuple(int index){
+            this.fields = new int[2];
+            this.relation_index = index;
+        }
+        public void setFields(int a, int b){
+            this.fields[0] = a;
+            this.fields[1] = b;
+        }
+        public int[] getFields(){
+            return this.fields;
+        }
+        public int getRelIndex(){
+            return this.relation_index;
+        }
+        public void setRelIndex(int i){
+            this.relation_index = i;
+        }
+        public void erase(){
+            this.fields = new int[2];
+        }
+    }
+
+    //block class
+    public static class Block implements Serializable{
+        private ArrayList<Tuple> tuples;
+
+        public Block(){
+            this.tuples = new ArrayList<Tuple>();
+        } 
+
+        public Block(Block b){
+            this.tuples = b.getTuples();
+        }
+        public boolean isFull(){
+            if(tuples.size()==0) return false;
+            if(tuples.size() == 8) return true;
+            return false;
+        }
+        public boolean addTuple(Tuple tuple) {
+            if (isFull()) {
+              System.err.print("ERROR: the block is full" + "\n");
+              return false;
+            }
+            this.tuples.add(new Tuple(tuple));
+            return true;
+        }
+	    public ArrayList<Tuple> getTuples(){
+            return this.tuples;
+        }
+        public boolean setTuples(ArrayList<Tuple> tuples, int start, int end) {
+            if((end - start) > 8) {
+                System.err.print("exceed space limit of the block" + "\n");
+                return false;
+            }
+            this.tuples.clear();
+            for(int i=start; i<end; i++){
+                this.tuples.add(new Tuple(tuples.get(i)));
+            }
+            return true;
+        }
+    }
+
+    //virtual disk
+    public static class Disk implements Serializable{
+        private ArrayList<ArrayList<Block>> tracks;
+        private long diskIOs;
+
+        public Disk(){
+            this.tracks = new ArrayList<ArrayList<Block>>(4);
+            for(int i = 0; i < 4; i++){
+                this.tracks.add(new ArrayList<Block>());
+            }
+            this.diskIOs = 0;
+        }
+        public void incrementDiskIO(int x){
+            this.diskIOs += x;
+        }
+        public long getDiskIO(){
+            return this.diskIOs;
+        }
+        public void resetDiskIO(){
+            this.diskIOs = 0;
+        }
+        public boolean setBlock(int relation_index, int index, Block b){
+            if(index < 0){
+                System.err.print("Block index is out of bounds.");
+                return false;
+            }
+            incrementDiskIO(1);
+            tracks.get(relation_index).set(index, b);
+            return true;
+        }
+        public ArrayList<ArrayList<Block>> getTracks(){
+            return this.tracks;
+        }
+        public Block getBlock(int rel_index, int block_index) {
+            if(block_index<0 || block_index >= tracks.get(rel_index).size())  {
+              System.err.print("block index " + block_index + " out of disk bound" + "\n");
+              return new Block();
+            }
+            incrementDiskIO(1);
+            return new Block(tracks.get(rel_index).get(block_index));
+        }
+        public boolean extendTrack(int relation_index, int index, Tuple t){
+            if(index < 0){
+                System.err.print("Block index is out of bounds.");
+                return false;
+            }
+            ArrayList<Block> track = tracks.get(relation_index);
+            int j = track.size();
+            if(index > j){
+                if(j > 0){
+                    while(!track.get(j-1).isFull()){
+                        track.get(j-1).addTuple(new Tuple(t));
+                    }
+                }
+                for(int i = j; i < index-1; i++) {
+                    track.add(new Block());
+                    while(!track.get(i).isFull()){
+                      track.get(i).addTuple(new Tuple(t));
+                    }
+                }
+                track.add(new Block());
+                track.get(index-1).addTuple(new Tuple(t));
+            }
+            return true;
+        }
+        public boolean retractTrack(int rel_index, int block_index) {
+            if(block_index < 0 || block_index >= tracks.get(rel_index).size()){
+              System.err.print("out of disk bound" + "\n");
+              return false;
+            }
+            tracks.get(rel_index).subList(block_index, tracks.get(rel_index).size()).clear();
+            return true;
+        }
+
+    }
+
+    //virtual memory
+    public static class Memory implements Serializable{
+        private Block[] blocks;
+
+        public Memory(){
+            blocks = new Block[NUM_OF_BLOCKS_IN_MEMORY];
+            for(int i = 0; i < NUM_OF_BLOCKS_IN_MEMORY; i++){
+                blocks[i] = new Block();
+            }
+        }
+        public int getMemorySize(){
+            return NUM_OF_BLOCKS_IN_MEMORY;
+        }
+        public Block getBlock(int index){
+            if(index < 0 || index > NUM_OF_BLOCKS_IN_MEMORY){
+                System.err.print("Block index out of bounds.");
+                return null;
+            }
+            return blocks[index];
+        }
+        public void setBlock(int index, Block b){
+            if(index < 0 || index > NUM_OF_BLOCKS_IN_MEMORY){
+                System.err.print("Block index out of bounds.");
+                return;
+            }
+            blocks[index] = b;           
+        }
+        public ArrayList<Tuple> getTuples(int start, int num_blocks){
+            if(start < 0 || start > NUM_OF_BLOCKS_IN_MEMORY){
+                System.err.print("Block index out of bounds.");
+                return new ArrayList<Tuple>();
+            }
+            if(num_blocks <= 0){
+                System.err.print("Not enough blocks");
+                return new ArrayList<Tuple>();
+            }
+            ArrayList<Tuple> all = new ArrayList<Tuple>();
+            for(int i = start; i < start+num_blocks; i++){
+                ArrayList<Tuple> temp = blocks[i].getTuples();
+                for(Tuple t : temp){
+                    all.add(t);
+                }
+            }
+            return all;
+        }
+        public boolean setTuples(int start, ArrayList<Tuple> tuples){
+            if(start < 0 || start > NUM_OF_BLOCKS_IN_MEMORY){
+                System.err.print("Block index out of bounds.");
+                return false;
+            }
+            int numBlocks = tuples.size()/8;
+            int additional = 0;
+            if(tuples.size() % 8 != 0){
+                additional = 1;
+            }
+            if(start+numBlocks+additional > NUM_OF_BLOCKS_IN_MEMORY){
+                System.err.print("Not enough memory space.");
+                return false;
+            }
+            int w1 = 0, w2 = 0, i;
+            for(i = start; i < start + numBlocks; i++){
+                w2 += 8;
+                blocks[i].setTuples(tuples, w1, w2);
+                w1 = w2;
+            }
+            if(additional != 0){
+                blocks[i].setTuples(tuples, w1, tuples.size());
+            }
+            return true;
+        }
+    }
+
+    //relation class
+    public static class Relation implements Serializable{
+        private Disk disk;
+        private Memory mem;
+        private String name;
+        private int index;
+
+        public Relation(Disk d, Memory m, String name, int index){
+            this.disk = d;
+            this.mem = m;
+            this.name = name;
+            this.index = index;
+        }
+        public int getNumBlocks(){
+            return disk.getTracks().get(this.index).size();
+        }
+        public boolean getBlock(int rel_index, int block_index)  {
+            if(block_index < 0 || block_index >= NUM_OF_BLOCKS_IN_MEMORY){
+                System.err.print("block index " + block_index + " out of bound in memory" + "\n");
+                return false;
+            }
+            Block b = disk.getBlock(rel_index, block_index);
+            if(!b.getTuples().isEmpty()) {
+                mem.setBlock(block_index, b);
+                return true;
+            }
+            return false;
+        }
+        public boolean setBlock(int rel_index, int block_index) {
+            if (block_index<0 || block_index >= NUM_OF_BLOCKS_IN_MEMORY){
+                System.err.print("block index" + block_index + " out of bound in memory" + "\n");
+                return false;
+            }
+            if(rel_index<0){
+                System.err.print("block index " + rel_index + " out of bound in relation" + "\n");
+                return false;
+            }
+            Tuple t = new Tuple(index);
+            t.erase();
+            if (disk.extendTrack(index,rel_index+1,t)){
+                return disk.setBlock(index, rel_index, mem.getBlock(block_index));
+            }
+            return false;
+        }
+        public boolean deleteBlocks(int startindex){
+            return disk.retractTrack(index, startindex);
+        }
+    }
+
+    //main class
+    public static void main(String[] args){
+        System.out.println("Starting components...");
+        Memory mem = new Memory();
+        Disk disk = new Disk();
+
+        //creating relation S(B, C)
+        Relation s = new Relation(disk, mem, "S(B,C)", 0);
+
+        //adding tuples to S's tuples to disk
+        Random rand = new Random();
+        Block b = new Block();
+        int blockindex = 0;
+        Set<Integer> primary = new HashSet<>();
+        int i = 0;
+        while(i < 5000){
+            int randInt = rand.nextInt(40000) + 10000;
+            if(primary.add(randInt)){
+                int randInt2 = rand.nextInt();
+                Tuple t = new Tuple(0);
+                t.setFields(randInt, randInt2);
+                if(b.isFull()){
+                    disk.setBlock(0, blockindex, b);
+                    blockindex++;
+                    b = new Block();
+                }
+                else{
+                    b.addTuple(t);
+                }
+                i++;
+            }
+        }
+        //creating relation R1(A, B)
+        Relation r1 = new Relation(disk, mem, "R1(A,B)", 1);
+        b = new Block();
+        i = 0;
+        int offset = primary.size();
+        ArrayList<Integer> c = new ArrayList<Integer>(primary);
+        blockindex = 0;
+        //loading this into disk, making sure some of the B values match with S relation
+        while(i < 500){
+            int randInt = c.get(rand.nextInt(offset));
+            int randInt2 = rand.nextInt();
+            Tuple t = new Tuple(1);
+            t.setFields(randInt2, randInt);
+            if(b.isFull()){
+                disk.setBlock(1, blockindex, b);
+                blockindex++;
+                b = new Block();
+            }
+            else b.addTuple(t);
+            i++;
+        }
+        while(i < 1000){
+            int randInt = rand.nextInt();
+            int randInt2 = rand.nextInt();
+            Tuple t = new Tuple(1);
+            t.setFields(randInt, randInt2);
+            if(b.isFull()){
+                disk.setBlock(1, blockindex, b);
+                blockindex++;
+                b = new Block();
+            }
+            else b.addTuple(t);
+            i++;
+        }
+
+
+    }
+}
